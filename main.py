d230f22febf7cb0787046d1b9cc6a8d8e998b363
AlexApps99 <30854646+AlexApps99@users.noreply.github.com>
2018-03-19 20:20:37 +1300

    Added /ping and /kick

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ bot.remove_command('help')
 
 @bot.event
 async def on_ready():
-	print('ShrekBot 0.1')
+	print('ShrekBot 0.2dev')
 	print('')
 	print('Logged in as:')
 	print(bot.user.name)
@@ -23,7 +23,7 @@ async def on_ready():
 
 @bot.command()
 async def help():
-	await bot.say('__**ShrekBot 0.2dev Commands:**__\n\n```css\n/help      : Shows help for commands\n/kill      : Kill minecraft avatars\n/clear     : Wipes the chat clean\n/choose    : Picks randomly between multiple choices\n/something : Random Stuff\n/zouss     : Zouss City\n/echo      : Echoes whatever you say```\n```\nIf you want to suggest more commands, visit the creator at:\nhttps://discord.gg/2anYtuD```')
+	await bot.say('__**ShrekBot 0.2dev Commands:**__\n\n```css\n/help      : Shows help for commands\n/kill      : Kill minecraft avatars\n/clear     : Wipes the chat clean\n/choose    : Picks randomly between multiple choices\n/something : Random Stuff\n/zouss     : Zouss City\n/echo      : Echoes whatever you say\n/ping      : Useful for testing Internet speed\n/kick      : For getting rid of annoyances```\n```\nIf you want to suggest more commands, visit the creator at:\nhttps://discord.gg/2anYtuD```')
 
 @bot.command()
 async def kill(*, mentioned = 'You'):
@@ -41,17 +41,25 @@ async def choose(*choices : str):
 async def something():
 	somethings = ['Somebody once told me the world was gonna roll me.\nI ain\'t the sharpest tool in the shed.', 'WHAT ARE YOU DOING IN MY SWAMP?!', 'Shrek is love, Shrek is life.']
 	await bot.say(random.choice(somethings))
-	
+
 @bot.command()
 async def zouss():
 	await bot.say('ζ ο υ ς ς    Ͼ ι τ ψ !')
-	
+
 @bot.command()
 async def ζουςς():
 	await bot.say('ζ ο υ ς ς    Ͼ ι τ ψ !')
 
 @bot.command()
 async def echo(*, message: str):
-    await bot.say(message)
+	await bot.say(message)
+
+@bot.command()
+async def ping():
+	await bot.say('Pong!')
+
+@bot.command(pass_context = True)
+async def kick(ctx, userName: discord.User):
+	await bot.kick(userName)
 
 bot.run(os.environ['TOKEN_DISCORD'])
