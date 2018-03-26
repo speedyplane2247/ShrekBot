df64aa1b0890bbfb806123bc73502e5bc4e0e3cd
AlexApps99 <30854646+AlexApps99@users.noreply.github.com>
2018-03-26 18:55:52 +1300

    Update main.py

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ async def kick(ctx, userName: discord.User):
 @bot.command()
 async def hex():
 	r = lambda: random.randint(0,255)
-	str('#%02X%02X%02X' % (r(),r(),r())) = hexcode
+	hexcode = '#%02X%02X%02X' % (r(),r(),r())
 	await bot.say(hexcode)
 	im = Image.new("RGB", (64,64), hexcode)
 	im.save("color.png")
