9da6b5f7af54722248fa5284747aeb30c70e3e06
AlexApps99 <30854646+AlexApps99@users.noreply.github.com>
2018-03-26 19:50:55 +1300

    Update main.py

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ async def kick(ctx, userName: discord.User):
 async def hex():
 	r = lambda: random.randint(0,255)
 	hexcode = '%02X%02X%02X' % (r(),r(),r())
-	rgbcode = str(tuple(int(hexcode[i:i+2], 16) for i in (0, 2 ,4))).lstrip('\(', '\)')
+	rgbcode = str(tuple(int(hexcode[i:i+2], 16) for i in (0, 2 ,4))).lstrip('\(').lstrip('\)')
 #	splitrgbcode = rgbcode.split(',')
 #	hsvcode = colorsys.rgb_to_hsv(splitrgbcode)
 	await bot.say('`Hex: #' + hexcode + '`\n`RGB: ' + rgbcode + '`')
