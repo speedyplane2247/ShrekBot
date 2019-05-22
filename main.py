9e9033c4578fb57dbd3cefb74d43a48bd3acb89a
AlexApps99 <alex.apps99@gmail.com>
2019-05-22 16:21:08 +1200

    Fixes maybe

--- main.py
+++ main.py
@@ -363,13 +363,14 @@ async def servers(ctx):
     '''
     servers = BOT.guilds
     servers.sort(key=lambda x: x.member_count)
-    await ctx.send('***Top servers with ShrekBot:***')
-    for x in servers[:10]:
-        await ctx.send('**{}**, **{}** Members'.format(x.name, x.member_count))
+    output = '***Top servers with ShrekBot:***\n'
+    for x in servers[10:]:
+        output += '**{}**, **{}** Members\n'.format(x.name, x.member_count)
     y = 0
     for x in BOT.guilds:
         y += x.member_count
-    await ctx.send('***Total number of ShrekBot users:** ***{}***!'.format(y))
-    await ctx.send('**Number of servers:** ***'+str(len(BOT.guilds))+'***!')
+    output += '**Total number of ShrekBot users:** ***{}***!\n'.format(y)
+    output += '**Number of servers:** ***'+str(len(BOT.guilds))+'***!'
+    await ctx.send(output)
 
 BOT.run(TOKEN_DISCORD)
